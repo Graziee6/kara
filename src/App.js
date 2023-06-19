4ce4784da4fe180f14fe0949300541717ac198d3
grace.fidele <gngrace10@gmail.com>
2023-06-19 08:45:05 +0200

    clear setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import Login from "./pages/Login/Login";
+import CarOwnerRegister from "./pages/CarOwnerRegister/CarOwnerRegister";
+import LinkOwner from "./pages/LinkOwner/LinkOwner";
+import RegisterCar from "./pages/RegisterCar/RegisterCar";
+import GetInfo from "./pages/GetCars/GetInfo";
+import AdminRegister from './pages/AdminRegister/AdminRegister';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<AdminRegister />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registerOwner" element={<CarOwnerRegister />} />
+        <Route path="/link" element={<LinkOwner />} />
+        <Route path="/registerCar" element={<RegisterCar />} />
+        <Route path="/getInfo" element={<GetInfo />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
